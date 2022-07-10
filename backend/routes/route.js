02f8f111ab95ec35b24943a3b7a1df26fe64ead5
Unnati Moralwar <unnatimoralwar@gmail.com>
2022-07-10 19:01:55 +0530

    getAll is added

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const apiController = require("../controllers/apiController.js");
+const data = require("../model/Unnati");
 
 router.post("/", apiController.addData);
 router.get("/", apiController.getData);
 router.put("/:id", apiController.putData);
+router.get("/getAll", async (req, res) => {
+  let obj = await data.find();
+  if (!obj) {
+    res.status(404).json({
+      msg: "no data",
+    });
+  }
+  return res.json(obj);
+});
 
 module.exports = router;
